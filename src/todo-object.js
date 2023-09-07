0ea2110980353878d464823528bd6b4a10a63a1b
Jack Sebben <jacksebben11@hotmail.com>
2023-09-07 17:35:19 -0400

    Project factory function created. This is where ToDo instances will be housed and where users can create instances of a project.

--- src/todo-object.js
+++ src/todo-object.js
@@ -8,4 +8,14 @@ const ToDo = (title, date, desc, priority) => {
 	return { getTitle, getDate, getDescription, getPriority };
 };
 
-export default ToDo;
+const Project = (title, date, desc, priority) => {
+	const getTitle = () => title;
+	const getDate = () => date;
+	const getDescription = () => desc;
+	const getPriority = () => priority;
+
+	console.log("ToDo Created...");
+	return { getTitle, getDate, getDescription, getPriority };
+};
+
+export { ToDo, Project };
