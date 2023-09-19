7f56c487072b801a765b0c2c6f06a0a1cb97405e
Jack Sebben <jacksebben11@hotmail.com>
2023-09-18 21:41:27 -0400

    This module will hold functions that will help determine the current folder a user is in. This will include functions for holding the users current state (the project a user is currently viewing), setting the current state (setting the project a user last clicked on as the current state) and getting the current state of the user's viewing.

--- src/logic/project-navigation.js
+++ src/logic/project-navigation.js
@@ -1,2 +1,17 @@
+// Object to keep track of what Project a user is currently viewing.
+let userState = {
+	currentProject: null,
+};
 
+// Event listener to set current Project
+function setCurrentProject(event) {
+	if (event.target.classList.contains("project")) {
+		userState.currentProject = event.target.id;
+		console.log("User is now in:", userState.currentProject);
+	}
+}
 
+// Function to check which Project the user is in
+function getCurrentProject() {
+	return userState.currentProject;
+}
